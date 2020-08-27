e8637c4e8ce130044bfe255bd516c612920a6a40
seagalputra <seagalputra@gmail.com>
2020-08-27 16:14:06 +0700

    adding example of lambdas

--- src/Lib.hs
+++ src/Lib.hs
@@ -281,3 +281,31 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ []       _        = []
 zipWith' _ _        []       = []
 zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f x y = f y x
+
+-- map takes a function and applies to every element in list, producing a new list.
+map' :: (a -> b) -> [a] -> [b]
+map' _ []       = []
+map' f (x : xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x : xs) | p x       = x : filter p xs
+                   | otherwise = filter p xs
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000, 99999 ..])
+  where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n | even n = n : chain (n `div` 2)
+        | odd n  = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1 .. 100]))
+
+addThree' :: (Num a) => a -> a -> a -> a
+addThree' = \x -> \y -> \z -> x + y + z
